f1f130681a0239ec7a30258661f6371d7fba0a19
hernanbeneventano <hernsnbeneventano19@gmail.com>
2026-09-16 15:26:14 -0300

    pequeños cambios

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 
 from core.views import health_check
 
 import core.views as views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"projects", views.ProjectViewSet, basename="project")
+router.register(r"tasks", views.TaskViewSet, basename="task")
 
 
 urlpatterns = [
     path('', health_check, name='health_check'),
     path("projects/", views.project_list, name="project_list"),
     path("tasks/", views.task_list, name="task_list"),
+    path("api/", include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,32 +1,34 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import JsonResponse
-
-def health_check(request):
-    return JsonResponse({"status": "ok", "service": "Proyecto Python 1"})
-
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
- 
- 
+
+
 def health_check(request):
     return JsonResponse({"status": "ok", "service": "TaskFlow API"})
- 
- 
+
+
 @api_view(["GET"])
 def project_list(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
- 
- 
+
+
 @api_view(["GET"])
 def task_list(request):
     tasks = Task.objects.select_related("project").prefetch_related("tags").all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.select_related("project").prefetch_related("tags").all()
+    serializer_class = TaskSerializer
